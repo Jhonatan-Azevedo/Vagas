7604b6e4ee42249ac5108d87b5c37a12802a17b6
Jhonatan Azevedo <dev.azevedo@outlook.com>
2022-04-06 12:20:09 -0300

    ADD: componente para informacoes de vagas

---
+++ src/components/comuns/IndicadorDados.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div :class="estilo">
+      <p>{{ titulo }}</p>
+      <h2>{{ indicador }}</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IndicadorDados",
+  props: ["titulo", "indicador", "bg", "color"],
+  computed: {
+    estilo() {
+      return `h-100 p-5 rounded-3 border ${this.bg} ${this.color}`;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/components/comuns/PesquisarVaga.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div class="h-100 p-5 bg-light border rounded-3">
+      <h2>Pesquisar Vagas</h2>
+      <div class="row">
+        <div class="col">
+          <div class="form-group">
+            <label>Título da Vaga</label>
+            <input
+              type="text"
+              class="form-control"
+              placeholder="Pesquise por pavalas chaves. Ex: 'C#', 'java', 'node', 'PHP'"
+            />
+            <small class="form-text text-muted"
+              >Informe palavras que estejam relacionadas ao título que você
+              procura</small
+            >
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <button class="btn btn-outline-dark mt-2">Buscar</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PesquisarVaga",
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
