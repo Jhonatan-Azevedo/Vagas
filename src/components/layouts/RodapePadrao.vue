29b7da642842f8f881a736dd1cf525e26b47ebbe
Jhonatan Azevedo <dev.azevedo@outlook.com>
2022-06-01 17:19:37 -0300

    ADD: link para linkedin

--- src/components/layouts/RodapePadrao.vue
+++ src/components/layouts/RodapePadrao.vue
@@ -1,13 +1,31 @@
 <template>
   <footer
-    class="border-top w-100 d-flex justify-content-center align-items-center"
+    class="
+      border-top
+      px-5
+      w-100
+      d-flex
+      justify-content-between
+      align-items-center
+    "
   >
     <a
       class="text-decoration-none link-secondary"
       href="https://github.com/Jhonatan-Azevedo"
       target="_blank"
     >
-      <i class="bi bi-github"></i> Jhonatan Azevedo Developer@{{ date }}
+      <i class="bi bi-github"></i> Github
+    </a>
+
+    <span class="text-secondary mx-5"
+      >Jhonatan Azevedo Developer@{{ date }}</span
+    >
+    <a
+      class="text-decoration-none link-secondary"
+      href="https://www.linkedin.com/in/jazevedodev/"
+      target="_blank"
+    >
+      <i class="bi bi-linkedin"></i> Linkedin
     </a>
   </footer>
 </template>
